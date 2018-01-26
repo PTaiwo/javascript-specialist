a818a89fbb6c8169b64676e9b6333613320d9024
PTaiwo <33854291+PTaiwo@users.noreply.github.com>
2018-01-26 18:49:11 -0500

    Variables and Math
    Finishing this chapter

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ window.onload = function(){
     document.getElementById("alertButton").addEventListener("click", alertResponse);
     
     document.getElementById("colorButton").addEventListener("click", colorResponse);
+    
+   
 }
 
 function firstResponse(e) {
@@ -27,3 +29,51 @@ function colorResponse(e) {
     
     document.getElementById("colorButton").style.color = "red";
 }
+
+
+window.onload = function()
+{
+   document.getElementById("addingButton").addEventListener("click", addNumbers); 
+    
+   document.getElementById("multiplyButton").addEventListener("click", multiplyNumbers); 
+    
+   document.getElementById("divisionButton").addEventListener("click", divideNumbers); 
+}
+
+function addNumbers(e) {
+    
+var answer = "";
+    
+answer += + document.getElementById('inputOne').value + + document.getElementById('inputTwo').value
+    
+    alert(answer);
+}
+
+function multiplyNumbers(e) {
+    
+var answer = "";
+    
+answer += + document.getElementById('inputThree').value * document.getElementById('inputFour').value
+    
+    alert(answer);
+}
+
+function divideNumbers(e) {
+    
+var answer = "";
+    
+answer += + document.getElementById('inputFive').value / document.getElementById('inputSix').value
+    
+    alert(answer);
+}
+
+
+/*function addNumbers(e) {
+    
+    var answer = "";
+    var fn, ln;
+    fn = Number(document.getElementById('inputOne').value);
+    ln = Number(document.getElementById('inputTwo').value);
+    answer = (fn + ln);
+    alert(answer);
+} */
