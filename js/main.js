88105a36ae0e295e976b09269bc24bc945913490
PTaiwo <33854291+PTaiwo@users.noreply.github.com>
2018-01-26 11:54:27 -0500

    Chapter 1
    Completing chapter 1 exercises.

---
+++ js/main.js
@@ -0,0 +1,29 @@
+window.onload = function(){
+    
+    document.getElementById("consoleButton").addEventListener("click", firstResponse);
+    
+    document.getElementById("browserButton").addEventListener("click", secondResponse);
+    
+    document.getElementById("alertButton").addEventListener("click", alertResponse);
+    
+    document.getElementById("colorButton").addEventListener("click", colorResponse);
+}
+
+function firstResponse(e) {
+    console.log("Thanks for clicking he console button!");
+}
+
+function secondResponse(e) {
+    
+    document.write("Thanks for clicking the browser button");
+}
+
+function alertResponse(e) {
+    
+    alert("Learning the magic of JavaScript, keep going!!!");
+}
+
+function colorResponse(e) {
+    
+    document.getElementById("colorButton").style.color = "red";
+}
