84776ab5763ed46b04d82a9f150b83984a079c01
PTaiwo <33854291+PTaiwo@users.noreply.github.com>
2018-01-27 00:58:27 -0500

    Chapter 3
    Finishing chapter 3.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* These functions are for the first lab. */
 window.onload = function(){
     
     document.getElementById("consoleButton").addEventListener("click", firstResponse);
@@ -30,7 +31,7 @@ function colorResponse(e) {
     document.getElementById("colorButton").style.color = "red";
 }
 
-
+/*These functions are for the second lab */
 window.onload = function()
 {
    document.getElementById("addingButton").addEventListener("click", addNumbers); 
@@ -66,14 +67,61 @@ answer += + document.getElementById('inputFive').value / document.getElementById
     
     alert(answer);
 }
+/*These functions are for the second lab */
+window.onload = function()
+{
+   alert("Let's play! Think of a rock, paper, or scissors!"); document.getElementById('playButton').addEventListener('click', randomNumber);
+}
 
-
-/*function addNumbers(e) {
+function randomNumber(e)
+{
+    var computerAnswer = Math.random();
     
-    var answer = "";
-    var fn, ln;
-    fn = Number(document.getElementById('inputOne').value);
-    ln = Number(document.getElementById('inputTwo').value);
-    answer = (fn + ln);
-    alert(answer);
-} */
+    if (computerAnswer <= 0.33)
+        {
+            alert("Computer chose rock!");
+        } else if (computerAnswer >0.33 && computerAnswer <= 0.66)
+        {
+            alert("Computer chose paper!");
+        } else 
+        {
+            alert("Computer chose scissors");         
+        }
+};
+
+/* These functions are for the swith lab */
+window.onload = function()
+{
+    document.getElementById('beverageButton').addEventListener('click', nameBeverage);
+}
+
+function nameBeverage(e)
+{
+    var choice;
+    var yourBeverage = prompt("What's your beverage of choice?");
+    
+    switch(yourBeverage.toLowerCase()) {
+        case "beer":
+        choice = "Good choices on tap, IPAs are in.";
+        break;
+        case "whiskey":
+        choice = "Bourbon, Scotch or Single Malt?";
+        break;
+        case "wine":
+        choice = "Red or White? California or Foreign?";
+        break;
+        case "gin":
+        choice = "Lime or Tonic?";
+        break;
+        case "vodka":
+        choice = "Cranberry or Tonic?";
+        break;
+        case "cognac":
+        choice = "VS, VSOP or XO, neat I assume?";
+        break;
+        default:
+        choice = "You need to step up your drink game!!!";
+    }
+    document.getElementById('drinks').innerHTML = "<h3>" + choice + "</h3>";
+}
+
